02ed28c5d436d39b6f1469b9f2d73e0e455c4663
Kotoki <Kotoki1337@gmail.com>
2023-12-10 20:04:01 +0800

    Tag

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,6 @@ onMounted(async () => {
 	cards.value = await fetchCardsData();
 	countryList.value = await countCountry(bank.value, cards.value);
 
-	const toLowerCase = (str) => str.toLowerCase();
-
 	await nextTick();
 	initTE({ Sticky, Ripple, Collapse, Dropdown });
 });
@@ -43,17 +41,21 @@ onMounted(async () => {
 					type="button"
 					class="inline-block rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg motion-reduce:transition-none"
 					style="background-color: #333"
-				><a href="https://github.com/HarukaKinen/Cardentify" target="_blank">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-4 w-4"
-						fill="currentColor"
-						viewBox="0 0 24 24"
+				>
+					<a
+						href="https://github.com/HarukaKinen/Cardentify"
+						target="_blank"
 					>
-						<path
-							d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-						/>
-					</svg></a>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-4 w-4"
+							fill="currentColor"
+							viewBox="0 0 24 24"
+						>
+							<path
+								d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+							/></svg
+					></a>
 				</button>
 			</div>
 			<div class="border-b border-gray-700"></div>
@@ -69,9 +71,7 @@ onMounted(async () => {
 				<br />
 				<h1 class="text-2xl">获取 Apple Pay 高清卡面</h1>
 				<br />
-				<h2 class="text-xl">
-					如果你拥有配备 Touch ID 的 Mac
-				</h2>
+				<h2 class="text-xl">如果你拥有配备 Touch ID 的 Mac</h2>
 				<p class="text-sm">
 					请参见美卡论坛的<a
 						href="https://www.uscardforum.com/t/topic/29408"
@@ -211,58 +211,47 @@ onMounted(async () => {
 													>
 														{{ card.issuer.name }}
 													</p>
-													<ul class="w-full">
-														<li
-															v-if="card.bin"
-															class="w-full border-b-2 border-neutral-100 border-opacity-50 px-6 py-3"
-														>
+													<div>
+														<span v-if="card.bin">
 															<span
 																v-for="(
 																	bin, index
 																) in card.bin"
 																:key="index"
+																class="inline-block whitespace-nowrap rounded-full bg-primary-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-primary-700"
 															>
-																{{ bin
-																}}<span
-																	v-if="
-																		index <
-																		card.bin
-																			.length -
-																			1
-																	"
-																	>,
-																</span>
+																{{ bin }}
 															</span>
-														</li>
-														<li
+														</span>
+														<span
 															v-if="
 																card.card.type
 															"
-															class="w-full border-b-2 border-neutral-100 border-opacity-50 px-6 py-3"
+															class="inline-block whitespace-nowrap rounded-full bg-secondary-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-secondary-800"
 														>
 															{{ card.card.type }}
-														</li>
-														<li
+														</span>
+														<span
 															v-if="
 																card.card.level
 															"
-															class="w-full border-b-2 border-neutral-100 border-opacity-50 px-6 py-3"
+															class="inline-block whitespace-nowrap rounded-full bg-success-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-success-700"
 														>
 															{{
 																card.card.level
 															}}
-														</li>
-														<li
+														</span>
+														<span
 															v-if="
 																card.card.brand
 															"
-															class="w-full border-neutral-100 border-opacity-50 px-6 py-3"
+															class="inline-block whitespace-nowrap rounded-full bg-danger-100 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-danger-700"
 														>
 															{{
 																card.card.brand
 															}}
-														</li>
-													</ul>
+														</span>
+													</div>
 												</div>
 											</div>
 										</div>
