781ae063c27936763c2c8ce6f048afefced29612
Kotoki <Kotoki1337@gmail.com>
2023-12-19 22:17:13 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -69,14 +69,14 @@ onMounted(async () => {
 					aria-current="page"
 					>Cardentify</a
 				>
-				<button
-					type="button"
-					class="inline-block rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg motion-reduce:transition-none"
-					style="background-color: #333"
+				<a
+					href="https://github.com/HarukaKinen/Cardentify"
+					target="_blank"
 				>
-					<a
-						href="https://github.com/HarukaKinen/Cardentify"
-						target="_blank"
+					<button
+						type="button"
+						class="inline-block rounded px-3 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg motion-reduce:transition-none"
+						style="background-color: #333"
 					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -86,9 +86,9 @@ onMounted(async () => {
 						>
 							<path
 								d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-							/></svg
-					></a>
-				</button>
+							/>
+						</svg></button
+				></a>
 			</div>
 			<div class="border-b border-gray-700"></div>
 		</div>
@@ -253,10 +253,12 @@ onMounted(async () => {
 																class="h-full flex flex-col justify-between items-center"
 															>
 																<span></span>
+																<span></span>
 																<span
 																	>透过 Github
 																	打开原文件</span
 																>
+																<span>来自 {{ card.source }}</span>
 																<span></span>
 															</div>
 														</div>
